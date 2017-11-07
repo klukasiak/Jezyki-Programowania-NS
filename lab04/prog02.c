a5fa30315443e2c32e5be514a5566cfcf376d027
klukasiak <klukasiak@sigma.ug.edu.pl>
2017-11-07 20:52:09 +0100

    updated

--- lab04/prog02.c
+++ lab04/prog02.c
@@ -2,9 +2,16 @@
 
 int main() {
   int n;
+  int jest_pierwsza;
   printf("Podaj liczbe calkowita: ");
   scanf("%d" , &n);
-  /* sprawdź czy n jest liczbą pierwszą */
+  for(int i = 2; i<n; i++){
+	  if (n%i==0){
+		  if (i==n) jest_pierwsza = 1;
+		  else jest_pierwsza = 0;
+		  break;
+	  }
+  }
   printf("Podana liczba '%d' jest %s\n",
     n, jest_pierwsza ? "pierwsza" : "złożona");
   return 0;
